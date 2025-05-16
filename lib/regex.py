1826cc4c901e6c523c1f84fc5bc996e1fbbe5c01
Joel Mwaga <mwagahjoel14@gmail.com>
2025-05-17 01:05:40 +0300

    learnt about r

--- lib/regex.py
+++ lib/regex.py
@@ -1,13 +1,12 @@
 import re
 
-# NOTE: There are only a few tests included, so multiple solutions will work.
-# Feel free to encourage students to find oversights and add tests to this lab!
-
-name = r""
+# Allow for names like D'Angelo (single uppercase letter after apostrophe)
+# Fix: allow for just one uppercase letter after apostrophe/hyphen/space
+name = r"^[A-Z][a-z]*(?:[ '-][A-Z][a-z]*)*$"
 name_regex = re.compile(name)
 
-phone_number = r""
+phone_number = r"^(?:\d{10}|\d{3}-\d{3}-\d{4}|\(\d{3}\) \d{3}-\d{4})$"
 phone_regex = re.compile(phone_number)
 
-email_address = r""
+email_address = r"^[a-z]+(?:\.[a-z0-9]+)*@[a-z]+\.[a-z]+$"
 email_regex = re.compile(email_address)
